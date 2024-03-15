3c2ab7c27a7fc8bc4eb4a3653c4609adddebc9b8
yashika2707 <yashikavelu@gmail.com>
2024-03-15 20:55:32 +0530

    add some change in search box

--- js/search.js
+++ js/search.js
@@ -1,40 +1,48 @@
-let availablekeywords = [
-    'Home',
-    'About',
-    'Gallery',
-    'Features', 
-    'Services',
-    'Download Brochure',
-    'contact',
-    'Why Choose Us',                            
-] ;
-const resultsBox = document.querySelector(".result-box");
-const inputBox = document.getElementById("input-box");
+ let availablekeywords = [
+    //  'Home',
+    //  'About',
+    //   'Features',
+    //   'Services',
+    //   'Gallery',
+    //  'contact',
+    //  'Download Brochure',
+    '<a href="index.html"> Home </a>',
+    '<a href="about.html"> About </a>',
+    '<a href="feature.html"> Features </a>',
+     '<a href="service.html"> Services </a>',
+     '<a href="gallery.html"> Gallery </a>',
+     '<a href="contact.html"> contact </a>',
+     '<a href="brochure.pdf"> Download Brochure </a>',
+         ] ;
+ const resultsBox = document.querySelector(".result-box");
+ const inputBox = document.getElementById("input-box");
 
 inputBox.onkeyup = function(){
     let result = [];
-    let input = inputBox.value;
-    if(input.length){
+     let input = inputBox.value;
+     if(input.length){
         result = availablekeywords.filter((keyword)=>{
              return keyword.toLowerCase().includes(input.toLowerCase());
         });
-        console.log(result);
+         console.log(result);
+   }
+       display(result);
+         if(!result.length){
+         resultsBox.innerHTML = '';
+  }
     }
-      display(result);
+ function display(result){
+     const content = result.map((list)=>{
+         return "<li onclick=selectInput(this)>"+ list + "</li>";
+     });
+
+     resultsBox.innerHTML = "<ul>"+ content.join('') +"</ul>";
+ }
+    function selectInput(list){
+       inputBox.value = list.innerHTML;
+       resultsBox.innerHTML ='';
+
+    }
     
-    if(!result.length){
-        resultsBox.innerHTML = '';
-    }
-   }
-function display(result){
-    const content = result.map((list)=>{
-        return "<li onclick=selectInput(this)>"+ list + "</li>";
-    });
-
-    resultsBox.innerHTML = "<ul>"+ content.join('') +"</ul>";
-}
-   function selectInput(list){
-      inputBox.value = list.innerHTML;
-      resultsBox.innerHTML ='';
-
-   }
+   
+    
